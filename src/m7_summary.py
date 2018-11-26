3df3e20602c0b5cba3c2e764cc040dda94023059
wenningr <wenningr@rose-hulman.edu>
2018-11-26 16:11:35 -0500

    Done

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,16 +2,16 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Greg Wenning.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Write code that accomplishes the following (and ONLY the following),
 #   in the order listed:
 #
@@ -43,3 +43,22 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+window.delay(30)
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen = rg.Pen('blue', 3)
+blue_turtle.speed = 5
+
+blue_turtle.pen_down()
+blue_turtle.left(90)
+blue_turtle.forward(200)
+
+blue_turtle.pen_up()
+blue_turtle.go_to(rg.Point(100,-40))
+blue_turtle.pen_down()
+blue_turtle.pen=rg.Pen('green',10)
+blue_turtle.right(180)
+blue_turtle.forward(150)
+
+window.close_on_mouse_click()
